0e2df76cd8c608f2661092604f5f93c144e13834
jaimeserranodev <jaime.serrano.dev@gmail.com>
2024-01-19 17:08:15 +0000

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.onClose);
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    },[isOpen, onOpen]);
+
     return ( 
         <div className="p-4">
-            <Button>Click Me</Button>
+            Root Page
         </div>
-        
-        )
+        );
     }
-    
+
+    export default SetupPage;
