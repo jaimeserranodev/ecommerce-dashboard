6d984f9beec042d45a0fcdc9e1f41653b784fdd8
jaimeserranodev <jaime.serrano.dev@gmail.com>
2024-01-22 12:25:02 +0000

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
-import { useStoreModal } from "@/hooks/use-store-modal";
 import { useEffect } from "react";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
 
 const SetupPage = () => {
     const onOpen = useStoreModal((state) => state.onOpen);
-    const isOpen = useStoreModal((state) => state.onClose);
+    const isOpen = useStoreModal((state) => state.isOpen);
 
     useEffect(() => {
         if (!isOpen) {
